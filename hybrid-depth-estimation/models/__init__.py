85f5ba5f01a3790e81da1260ee49ed75a752d095
Sachin R <78604973+Sachin2495@users.noreply.github.com>
2025-02-16 05:41:50 +0000

    Update project structure and move files to new directories

--- hybrid-depth-estimation/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# FILE: /hybrid-depth-estimation/models/__init__.py
-from .midas_model import MiDaSModel
-from .adabins_model import preprocess_adabins_input
-from .dpt_large_model import load_dpt_large_model, preprocess_dpt_input, predict_dpt_depth
